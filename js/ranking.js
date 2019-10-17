e14447c2ea78afdb954c4be9e1f16e122509ac69
charlie <402057@gmail.com>
2019-10-17 19:55:20 +0800

    new botton hide

--- js/ranking.js
+++ js/ranking.js
@@ -1,3 +1,19 @@
-fuction = addParticipants{
+var userData={
+    userName:null,
+    acTimes:0,
+    commitTimes:0,
+    attendStatus:false,
+}
+
+//window.onload=callUserRank
+
+/*fuction=callUserRank{
     if()
 }
+*/
+
+//參加排名後按鈕隱藏
+$('#attend').click(function(){
+    $('#attend').hide()
+    userData.attendStatus=true
+})
