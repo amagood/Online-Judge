3bb661e3bc042452deacecc0ac0209a2831fb47b
charlie <402057@gmail.com>
2019-10-25 12:03:12 +0800

    build by jquery

--- js/rankingTestData.js
+++ js/rankingTestData.js
@@ -1,8 +1,8 @@
 //測試用的js檔
 
 var n001userData={
-    n001Name:aaa,
-    n001ACTimes:0,
-    n001CommitTimes:0,
-    n001AttendStatus:true,
+  n001Name:aaa,
+  n001ACTimes:0,
+  n001CommitTimes:0,
+  n001AttendStatus:true,
 }
