260cdc91d33cfab008726898758e9cd0b50882c0
charlie <402057@gmail.com>
2019-10-27 18:13:58 +0800

    add fake data to test

--- js/ranking.vue
+++ js/ranking.vue
@@ -1,16 +1,30 @@
 <script>
+  var fakeUserData = [
+    {"Name": "aaa",
+      "ACTimes": "0",
+      "CommitTimes": "0",
+      "rank": "1",
+      "AttendStatus": "true",},
+    {"Name": "aaa",
+      "ACTimes": "0",
+      "CommitTimes": "0",
+      "rank": "1",
+      "AttendStatus": "true",}
+  ]
   //----app1 post請求的發送值, hash值還不知----
   var rankAction = {
     "action": "rank",
     "hash": "A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9"
-  },
+  }
 
   //----app2 post請求的發送值, 改變學生參加排名狀態(預設應該為notAttend)----
   var attendRankList = {
     "action": "attend_rank",
     "attendStatus": "attend",
     "hash": "A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9"
-  },
+  }
+  var app1
+  var app2
 
   export default {
 
@@ -27,17 +41,22 @@
         }
       },
       created: {
+        fakeGetRankList(){
+          this.userData = fakeUserDate
+        }
+        /*真正用到的function
         getRankList(){
           axios.post("https://whereshouldIconnect",JSON.stringify(rankAction))
             .then(function(response){
-              this.userData = JSON.parse(response)
               console.log(response.data)
               console.log(response.status)
               console.log(response.statusText)
               console.log(response.headers)
               console.log(response.config)
+              this.userData = JSON.parse(response)
             })
         }
+        */
       } 
     }),
 
@@ -51,7 +70,7 @@
 
       }
     })
-    
+
   }
 </script>
 
