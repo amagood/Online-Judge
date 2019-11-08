23abfb109d5ad0373f0ae7938eac0a30d5912626
charlie <402057@gmail.com>
2019-11-08 16:48:55 +0800

    add navbar folder,attend ranklist button

--- js/ranking.js
+++ js/ranking.js
@@ -1,51 +1,43 @@
-//----app1 post請求的發送值, hash值還不知----
-var rankAction = {
-  action: "rank",
-  hash: "A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9"
-}
-
-//----app2 post請求的發送值, 改變學生參加排名狀態(預設應該為notAttend)----
-var attendRankList = {
-  action: "attend_rank",
-  attendStatus: "attend",
-  hash: "A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9"
-}
-
-//----執行創建排名列表----
+//----執行創建排名列表, 連線後把userData內資料移除,註解function換掉----
 var app1 = new Vue({
   el: "#app1",
   data: {
+    rankAction: {
+      action: "rank",
+      hash: "A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9"
+    },
     userData: [
       {
         name: "aaa",
-        ACTimes: "0",
-        commitTimes: "0",
-        rank: "1",
-        AttendStatus: "true"
+        ACTimes: 0,
+        commitTimes: 0,
+        rank: 1,
+        AttendStatus: true
       },
       {
         name: "aaa22",
-        ACTimes: "1",
-        commitTimes: "3",
-        rank: "2",
-        AttendStatus: "true"
+        ACTimes: 1,
+        commitTimes: 3,
+        rank: 2,
+        AttendStatus: true
       }
     ]
+    /*真正用到的userData
+    userData: []
+    */
   },
   created: function() {
-    this.aaa()
-    //this.getRankList()
+    this.getRankList()
   },
   methods: {
-    aaa(){
+    getRankList(){
       for(let i=0; i<2; i++){
         if(this.userData[i].commitTimes == 0)
-          this.userData[i].passRate = 0;
+          this.userData[i].passRate = 0
         else
           this.userData[i].passRate = ((this.userData[i].ACTimes*100)/this.userData[i].commitTimes).toFixed(2)
       }
     }
-
     /*真正用到的function
     getRankList(){
       axios.post("https://whereshouldIconnect",JSON.stringify(rankAction))
@@ -56,30 +48,78 @@ var app1 = new Vue({
           console.log(response.headers)
           console.log(response.config)
           this.userData = response.userData
-          for(let i=0; i<100; i++){
-            this.userData[i].passRate = (this.userData[i].ACTimes*100)/this.userData.commitTimes
-          }
         })
+      for(let i=0; i<2; i++){
+        if(this.userData[i].commitTimes == 0)
+          this.userData[i].passRate = 0
+        else
+          this.userData[i].passRate = ((this.userData[i].ACTimes*100)/this.userData[i].commitTimes).toFixed(2)
+      }
     }
     */
   },
 })
 
-//----執行使用者是否加入排名----
-/*
+//----執行使用者是否加入排名, 連線後註解要換掉----
 var app2 = new Vue({
   el: "#app2",
   data: {
-
+    isShow: true,
+    attendRank: {
+      action: "attend_rank",
+      attendStatus: "attend",
+      hash: "A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9"
+    },
+    notAttendRank: {
+      action: "attend_rank",
+      attendStatus: "notAttend",
+      hash: "A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9"
+    },
   },
-  mounted: {
-
+  methods: {
+    sendAttendMSG(){
+      alert("Congratulations! You success attend!")
+      this.isShow = false
+    },
+    sendNotAttendMSG(){
+      alert("Congratulations! You success attend!")
+      this.isShow = false
+    },
+    /*真正用到的function
+    sendAttendMSG(){
+      axios.post("https://whereshouldIconnect",JSON.stringify(attendRank))
+      .then(function(response){
+        console.log(response.data)
+        console.log(response.status)
+        console.log(response.statusText)
+        console.log(response.headers)
+        console.log(response.config)
+        if(response.status == "success"){
+          alert("Congratulations! You successful attend!")
+        }
+        else{
+          alert("Sorry! Maybe next time.")
+        }
+      })
+      this.isShow = false
+    },
+    sendNotAttendMSG(){
+      axios.post("https://whereshouldIconnect",JSON.stringify(notAttendRank))
+      .then(function(response){
+        console.log(response.data)
+        console.log(response.status)
+        console.log(response.statusText)
+        console.log(response.headers)
+        console.log(response.config)
+        if(response.status == "success"){
+          alert("What a pity! Attend 一下啦!")
+        }
+        else{
+          alert("Sorry! Maybe next time.")
+        }
+      })
+      this.isShow = false
+    }
+    */
   }
 })
-*/
-
-/*參加排名後按鈕隱藏
-$("#attend").click(function(){
-  $("#attend").hide()
-  userData.attendStatus = true
-})*/
