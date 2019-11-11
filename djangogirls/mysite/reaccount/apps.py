28833de6dfc38d2d9ad32660df3b56f3488ce1ff
YuHao <howard50209880522@gmail.com>
2019-11-11 14:27:30 +0800

    Account_Login V2
    djangogirls/mysite/reaccount/Account_Login_View
                                                        /Account_Login

--- djangogirls/mysite/reaccount/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReaccountConfig(AppConfig):
-    name = 'reaccount'
--- djangogirls/mysite/trips/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Post
-admin.site.register(Post)
-
