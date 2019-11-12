19be676b919b41a5a30341c84f41816aeca7901f
YuHao <howard50209880522@gmail.com>
2019-11-12 11:47:02 +0800

    androidVersionFrontEnd
    Finish android app(link to google now)

--- djangogirls/mysite/reaccount/view.py
+++ djangogirls/mysite/reaccount/view.py
@@ -14,4 +14,5 @@ def responseLoginStatus(request):
     if request.method == "POST":
         req=json.loads(request.body)
         data=reaccount.Account_Login_View.responseLoginStatus(req['account'],req['password'])
-    return HttpResponse(data)
+        return HttpResponse(data)
+    return HttpResponse('WTF')    
