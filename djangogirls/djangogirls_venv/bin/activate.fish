81f1eda9f72189c144fbee8647e48b626f688bee
YuHao <howard50209880522@gmail.com>
2019-11-12 11:47:02 +0800

    Account_Login V4
    djangogirls/mysite/reaccount/Account_Login_View
                                                        /Account_Login

--- djangogirls/djangogirls_venv/bin/activate.fish
+++ djangogirls/djangogirls_venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tsengyuhao/Public/Repo/djangogirls/djangogirls_venv"
+set -gx VIRTUAL_ENV "/Users/tsengyuhao/Public/Repo/OnlineJudge/djangogirls/djangogirls_venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
