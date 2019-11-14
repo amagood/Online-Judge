9961c23f5dcad96dee7d45a5aa86c0ef0c46399e
shin-han <hannah880708@gmail.com>
2019-11-14 18:58:42 +0800

    change table class to table-hover

--- QLibrary/js/studentProblem.js
+++ QLibrary/js/studentProblem.js
@@ -1,10 +1,3 @@
 window.onload = () => {
     new Vue({ el: '#app' })
 }
-function titleLinkOver(e){
-    e.style.color="DodgerBlue";
-    e.style.fortWeight="bold";
-}
-function titleLinkOut(e){
-    e.style.color="black";
-}
