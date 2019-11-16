bf337ac189923bc0c96b13ecf2d390e16c40e196
shin-han <hannah880708@gmail.com>
2019-11-17 03:33:30 +0800

    change file format

---
+++ js/studentProblem.js
@@ -0,0 +1,52 @@
+window.onload = () => {
+  new Vue({ el: '#app' })
+}
+
+window.onload = () => {
+  new Vue({
+    el: '#app-1',
+    data() {
+      return {
+        fields: [
+          {
+            key: "title",
+            label: "Title",
+            sortable: true
+          },
+          {
+            key: "target",
+            label: "Target",
+            sortable: true
+          },
+          {
+            key: "degree",
+            label: "Degree",
+            sortable: true
+          },
+          {
+            key: "pp",
+            label: "Percentage Passing",
+            sortable: true
+          },
+          {
+            key: "respondent",
+            label: "Respondent",
+            sortable: true
+          },
+          {
+            key: "time",
+            label: "Input Time",
+            sortable: true
+          }
+        ],
+        items: [
+          { title: "question001", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20190101" },
+          { title: "question002", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20180101" },
+          { title: "question003", target: "count", degree: "hard", pp: "50%", respondent: "100", time: "20170101" },
+          { title: "question004", target: "array", degree: "easy", pp: "50%", respondent: "100", time: "20190501" },
+          { title: "question005", target: "function", degree: "midium", pp: "50%", respondent: "100", time: "20190131" }
+        ]
+      }
+    }
+  })
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-README
+### 2019/11/16 Commit:add hover,select row 
+
+### 2019/10/27 Commit:new studentProblem page/new teacherProblem page
+    studentProblem.html
+    teacherProblem.html
+	add:navbar,Question sort button
+	undone:sort & changePage button link, table add frame
