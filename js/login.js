c60103a0c20b8b4255f37b6f902573fc3362c7c8
k435467 <k435467@gmail.com>
2019-11-16 16:05:51 +0800

    Validation
    	maxlength=20
    	pattern=[A-Za-z0-9].{6,}
    	show content after onload

--- js/login.js
+++ js/login.js
@@ -2,7 +2,14 @@
 var app1 = new Vue({
   el: "#app1",
   data: {
+    showContent: false,
     showFocus1: false,
     showFocus2: false
   }
 })
+
+
+window.onload = function() {
+  document.getElementsByTagName("html")[0].style.visibility = "visible";
+  this.app1.showContent = true;
+}
--- login.md
+++ login.md
@@ -1,2 +1,7 @@
+
+### 2019/11/16 Validation
+	maxlength=20
+	pattern=[A-Za-z0-9].{6,}
+	show content after onload
 ### 2019/11/15 Login First Commit
 	login.html css js
