89e4ef0f58869d31695438a130c857fdf9b7bda6
Will Shen <willshen0159@gmail.com>
2019-11-16 15:06:22 +0800

    New version of CodeRunner satisfying the doc
    Using folder to distinguish different id(submit).After compiling and
    execution, the folder in docker will be removed.

--- CodeRunner.py
+++ CodeRunner.py
@@ -5,20 +5,8 @@ def RunCode(id, language):
     with open("config.json") as file:
         config = json.load(file)
     containerId = config["containerId"]
-    code = id + "_code"
-    input = id + "_input"
-    output = id + "_output"
-    if language == "c":
-        code += ".c"
-    elif language == "c++":
-        code += ".cpp"
-    elif language == "python":
-        code += ".py"
-    else:
-        return 0
-    os.system("docker cp ./" + code + " " + containerId + ":/")
-    os.system("docker cp ./" + input + " " + containerId + ":/")
-    os.system("docker exec " + containerId + " bash -c \"./runCode.sh " \
-    + id + " " + language + "\"")
-    os.system("docker cp " + containerId + ":/" + output + " ./")
+    os.system("docker cp ./" + id + " " + containerId + ":/")
+    os.system("docker exec " + containerId + " bash ./runCode.sh " + id + " " + language)
+    os.system("docker cp " + containerId + ":/" + id + "/output.txt " + "./" + id)
+    os.system("docker exec " + containerId + " rm -r ./" + id)
     return 0
--- runCode.sh
+++ runCode.sh
@@ -2,39 +2,39 @@
 
 id=$1
 language=$2
-code=${id}"_code"
-input=${id}"_input"
-output=${id}"_output"
 
 if [ "${language}" == "c" ]; then
-	error=$(gcc -o ${id}.out ${code}.c 2>&1)
+	error=$(gcc -o ./${id}/a.out ./${id}/code.c 2>&1)
 	if [ "$?" == "1" ]; then
-		printf "CE\n" > ${output}
-		printf "${error}" >> ${output}
+		printf "CE\n" > ./${id}/output.txt
+		printf "${error}" >> ./${id}/output.txt
 		exit 0
 	fi
-	printf "\n" > ${output}
-	./${id}.out < ${input} >> ${output}
+	printf "\n" > ./${id}/output.txt
+	./${id}/a.out < ./${id}/input.txt >> ./${id}/output.txt
 	exit 0
+	
 elif [ "${language}" == "c++" ]; then
-	error=$(g++ -o ${id}.out ${code}.cpp 2>&1)
+	error=$(g++ -o ./${id}/a.out ./${id}/code.cpp 2>&1)
 	if [ "$?" == "1" ]; then
-		printf "CE\n" > ${output}
-		printf "${error}" >> ${output}
+		printf "CE\n" > ./${id}/output.txt
+		printf "${error}" >> ./${id}/output.txt
 		exit 0
 	fi
-	printf "\n" > ${output}
-	./${id}.out < ${input} >> ${output}
+	printf "\n" > ./${id}/output.txt
+	./${id}/a.out < ./${id}/input.txt >> ./${id}/output.txt
 	exit 0
+	
 elif [ "${language}" == "python" ]; then
-	error=$(python3 ${code}.py < ${input} 2>&1)
+	error=$(python3 ./${id}/code.py < ./${id}/input.txt 2>&1)
 	if [ "$?" == "1" ]; then
-		printf "CE\n" > ${output}
-		printf "${error}" >> ${output}
+		printf "CE\n" > ./${id}/output.txt
+		printf "${error}" >> ./${id}/output.txt
 		exit 0
 	fi
-	printf "\n" > ${output}
-	python3 ${code}.py < ${input} >> ${output}
+	printf "\n" > ./${id}/output.txt
+	python3 ./${id}/code.py < ./${id}/input.txt >> ./${id}/output.txt
 	exit 0
 fi
+
 exit 0
