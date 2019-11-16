0676eda6710d44fc217e2ba673a1c6e05f33fea2
shin-han <hannah880708@gmail.com>
2019-11-17 06:32:51 +0800

    teacher:add search bar,filter feature for button

--- js/studentProblem.js
+++ js/studentProblem.js
@@ -7,6 +7,9 @@ window.onload = () => {
     el: '#app-1',
     data() {
       return {
+        perPage: 20,//一頁幾行
+        currentPage: 1,//當前頁數
+        filter: null,//Search
         fields: [
           {
             key: "title",
@@ -44,8 +47,21 @@ window.onload = () => {
           { title: "question002", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20180101" },
           { title: "question003", target: "count", degree: "hard", pp: "50%", respondent: "100", time: "20170101" },
           { title: "question004", target: "array", degree: "easy", pp: "50%", respondent: "100", time: "20190501" },
-          { title: "question005", target: "function", degree: "midium", pp: "50%", respondent: "100", time: "20190131" }
+          { title: "question005", target: "function", degree: "midium", pp: "50%", respondent: "100", time: "20190131" }, 
         ]
+      }
+    },
+    computed: {
+      rows() {
+        return this.items.length
+      },
+      sortOptions() {
+        // Create an options list from our fields
+        return this.fields
+          .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
       }
     }
   })
--- js/teacherProblem.js
+++ js/teacherProblem.js
@@ -5,10 +5,14 @@ window.onload = () => {
 
 window.onload = () => {
   new Vue({
-    el: '#app',
+    el: '#app-1',
       data() {
         return {
-          clickAddClass:false,
+          perPage: 20,//一頁幾行
+          currentPage: 1,//當前頁數
+          //pageOptions: [5,10, 15],//使用者選擇當前頁數
+          filter: null,//Search
+          clickAddClass:false,//連結 Class button
           modes: ['multi', 'single', 'range'],//選擇模式多選、一次一行、shirt/ctrl選範圍
           fields: [//table 每列項目,用來取整列值
             {
@@ -47,7 +51,27 @@ window.onload = () => {
             { title: "question002", target:"loop" ,degree:"easy",pp:"50%",respondent:"100",time:"20180101" },
             { title: "question003", target:"count" ,degree:"hard",pp:"50%",respondent:"100",time:"20170101" },
             { title: "question004", target:"array" ,degree:"easy",pp:"50%",respondent:"100",time:"20190501" },
-            { title: "question005", target:"function" ,degree:"midium",pp:"50%",respondent:"100",time:"20190131" }
+            { title: "question005", target:"function" ,degree:"midium",pp:"50%",respondent:"100",time:"20190131" },
+            { title: "question006", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20190101" },
+            { title: "question007", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20180101" },
+            { title: "question008", target: "count", degree: "hard", pp: "50%", respondent: "100", time: "20170101" },
+            { title: "question009", target: "array", degree: "easy", pp: "50%", respondent: "100", time: "20190501" },
+            { title: "question0010", target: "function", degree: "midium", pp: "50%", respondent: "100", time: "20190131" },
+            { title: "question011", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20190101" },
+            { title: "question012", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20180101" },
+            { title: "question013", target: "count", degree: "hard", pp: "50%", respondent: "100", time: "20170101" },
+            { title: "question014", target: "array", degree: "easy", pp: "50%", respondent: "100", time: "20190501" },
+            { title: "question015", target: "function", degree: "midium", pp: "50%", respondent: "100", time: "20190131" },
+            { title: "question016", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20190101" },
+            { title: "question017", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20180101" },
+            { title: "question018", target: "count", degree: "hard", pp: "50%", respondent: "100", time: "20170101" },
+            { title: "question019", target: "array", degree: "easy", pp: "50%", respondent: "100", time: "20190501" },
+            { title: "question020", target: "array", degree: "easy", pp: "50%", respondent: "100", time: "20190501" },
+            { title: "question021", target: "function", degree: "midium", pp: "50%", respondent: "100", time: "20190131" },
+            { title: "question022", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20190101" },
+            { title: "question023", target: "loop", degree: "easy", pp: "50%", respondent: "100", time: "20180101" },
+            { title: "question024", target: "count", degree: "hard", pp: "50%", respondent: "100", time: "20170101" },
+            { title: "question025", target: "array", degree: "easy", pp: "50%", respondent: "100", time: "20190501" },
           ],
           selectMode: 'range',//選擇模式，<b-table :select-mode="selectMode">
         }
@@ -66,6 +90,19 @@ window.onload = () => {
           this.fields.splice()
         }*/
       }
-    }//method end
+    },//method end
+    computed: {
+      rows() {
+        return this.items.length
+      },
+      sortOptions() {
+        // Create an options list from our fields
+        return this.fields
+          .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
+      }
+    }//computed end
   })
 }
