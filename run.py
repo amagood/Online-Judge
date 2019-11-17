777199db5b7dc484ee16fc5ac496883596b59ac6
barry0310 <aaaa111111732s418@gmail.com>
2019-11-17 15:55:38 +0800

    using docker v2
    container memory limit

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 
 def RunCode(id, language):
-    os.system("docker run -dit --rm -v /home/barry/" + id + ":/" + id + " --name " + id + " green2")
+    os.system("docker run -dit -m 100m --memory-swap 100m --rm -v /home/barry/" + id + ":/" + id + " --name " + id + " green2")
     os.system("docker exec " + id + " bash home/run.sh " + id + " " + language)
     os.system("docker stop " + id )
     return 0
