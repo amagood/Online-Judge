29ffca5b4a64730ff52092152c5e39f7e2f04d0b
k435467 <k435467@gmail.com>
2019-11-18 15:58:15 +0800

    Post
    	html js
    	post when click login button, and show detail in console

--- js/login.js
+++ js/login.js
@@ -1,10 +1,39 @@
 
+
+var loginObj = {
+  "action" : "login",
+  "account" : "",
+  "password" : ""
+}
+var postURL = "login/"
+var testMode = false
+var tmpObj = {};
+
+
 var app1 = new Vue({
   el: "#app1",
   data: {
     showContent: false,
     showFocus1: false,
     showFocus2: false
+  },
+  methods: {
+    loginPost() {
+      loginObj.account  = document.getElementById("inputAccount").value;
+      loginObj.password = document.getElementById("inputPassword").value;
+      axios.post(postURL, loginObj)
+        .then(function (response) {
+          console.log(response);
+          if (testMode)
+            tmpObj = JSON.parse(response.data.freeform);
+          else
+            tmpObj = response.data;
+          console.log(tmpObj);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
   }
 })
 
--- login.md
+++ login.md
@@ -1,4 +1,7 @@
 
+### 2019/11/18/ Post
+	html js
+	post when click login button, and show detail in console
 ### 2019/11/16 Validation
 	maxlength=20
 	pattern=[A-Za-z0-9].{6,}
