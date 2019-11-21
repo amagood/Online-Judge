19b98335f68d7a077d31af72ca2c36bbc87c746e
BeePunch <kevinappledog@gmail.com>
2019-11-21 19:35:36 +0800

    2019/11/21
    更改網頁html檔名稱；增加檔案上傳限制功能

--- js/index.js
+++ js/index.js
@@ -1,3 +1,53 @@
 window.onload = () => {
   new Vue({ el: '#app' })
 }
+
+var selectField = document.getElementById("id_exampleImput");
+function selectLanguage(selectLanguageValue) {
+	alert("selectLanguageValue");
+	if (selectLanguageValue == "1") {
+  		document.getElementById("id_selectFileType").innerHTML = '<input size="40" name="account" type="file" id="id_pdf" autocomplete="off" value="" required="" accept = ".c">'
+	}
+	else if (selectLanguageValue == "2"){
+		document.getElementById("id_selectFileType").innerHTML = '<input size="40" name="account" type="file" id="id_pdf" autocomplete="off" value="" required="" accept = ".cpp,.cc">'
+	}
+	else{
+		document.getElementById("id_selectFileType").innerHTML = '<input size="40" name="account" type="file" id="id_pdf" autocomplete="off" value="" required="" accept = ".py">'
+	}
+}
+
+var exampleInput = document.getElementById("id_exampleInput");
+
+exampleInput.onchange = function() {
+    if(this.files[0].size > 10*1024*1024){
+       alert("File is too big!");
+       this.value = "";
+    };
+};
+
+var exampleOutput = document.getElementById("id_exampleOutput");
+
+exampleOutput.onchange = function() {
+    if(this.files[0].size > 10*1024*1024){
+       alert("File is too big!");
+       this.value = "";
+    };
+};
+
+var exampleCode = document.getElementById("id_exampleCode");
+
+exampleCode.onchange = function() {
+    if(this.files[0].size > 100*1024*1024){
+       alert("File is too big!");
+       this.value = "";
+    };
+};
+
+var PDF = document.getElementById("id_pdf");
+
+PDF.onchange = function() {
+    if(this.files[0].size > 1024*1024){
+       alert("File is too big!");
+       this.value = "";
+    };
+};
