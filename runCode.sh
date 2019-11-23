5d7af04f9346422e54be558d8f1068afed422a99
Will Shen <willshen0159@gmail.com>
2019-11-24 01:44:07 +0800

    Add timeout to runCode.sh
    Add time out to runCode.sh. When timeout, "TLE" will print to
    output.txt.

--- runCode.sh
+++ runCode.sh
@@ -2,6 +2,7 @@
 
 id=$1
 language=$2
+maxTime=10
 
 if [ "${language}" == "c" ]; then
 	error=$(gcc -o ./${id}/a.out ./${id}/code.c 2>&1)
@@ -11,7 +12,12 @@ if [ "${language}" == "c" ]; then
 		exit 0
 	fi
 	printf "\n" > ./${id}/output.txt
-	./${id}/a.out < ./${id}/input.txt >> ./${id}/output.txt
+	timeout ${maxTime} ./${id}/a.out < ./${id}/input.txt >> ./${id}/output.txt
+	if [ "$?" == "124" ]; then
+		printf "TLE\n" > ./${id}/output.txt
+		printf "Timeout" >> ./${id}/output.txt
+		exit 0
+	fi
 	exit 0
 	
 elif [ "${language}" == "c++" ]; then
@@ -22,14 +28,24 @@ elif [ "${language}" == "c++" ]; then
 		exit 0
 	fi
 	printf "\n" > ./${id}/output.txt
-	./${id}/a.out < ./${id}/input.txt >> ./${id}/output.txt
+	timeout ${maxTime} ./${id}/a.out < ./${id}/input.txt >> ./${id}/output.txt
+	if [ "$?" == "124" ]; then
+		printf "TLE\n" > ./${id}/output.txt
+		printf "Timeout" >> ./${id}/output.txt
+		exit 0
+	fi
 	exit 0
 	
 elif [ "${language}" == "python" ]; then
-	output=$(python3 ./${id}/code.py < ./${id}/input.txt 2>&1)
-	if [ "$?" == "1" ]; then
+	output=$(timeout ${maxTime} python3 ./${id}/code.py < ./${id}/input.txt 2>&1)
+	status=$?
+	if [ "${status}" == "1" ]; then
 		printf "CE\n" > ./${id}/output.txt
 		printf "${output}" >> ./${id}/output.txt
+		exit 0
+	elif [ "${status}" == "124" ]; then
+		printf "TLE\n" > ./${id}/output.txt
+		printf "Timeout" >> ./${id}/output.txt
 		exit 0
 	fi
 	printf "\n" > ./${id}/output.txt
