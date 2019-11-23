5fc1b61b61a9cdef56046782e025598f875cf73a
Will Shen <willshen0159@gmail.com>
2019-11-24 03:01:35 +0800

    Nothing important, just modify TestSite

--- TestSite/CodeRunner.py
+++ TestSite/CodeRunner.py
@@ -8,5 +8,4 @@ def RunCode(id, language):
     os.system("docker cp ./" + id + " " + containerId + ":/")
     os.system("docker exec " + containerId + " bash ./runCode.sh " + id + " " + language)
     os.system("docker cp " + containerId + ":/" + id + "/output.txt " + "./" + id)
-    os.system("docker exec " + containerId + " rm -r ./" + id)
     return 0
