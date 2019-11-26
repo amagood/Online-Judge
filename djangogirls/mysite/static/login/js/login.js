4afcb7327c8f60cb3b0add3806effca91c63ca5c
YuHao <howard50209880522@gmail.com>
2019-11-26 11:36:23 +0800

    Login_Account V7.1
    upgrade html...

--- djangogirls/mysite/static/login/js/login.js
+++ djangogirls/mysite/static/login/js/login.js
@@ -11,28 +11,84 @@ var tmpObj = {};
 
 
 var app1 = new Vue({
+  delimiters: ['${', '}'],
   el: "#app1",
   data: {
     showContent: false,
-    showFocus1: false,
-    showFocus2: false
+    showFocusAcct: false,
+    showFocusPw: false,
+    focusAcctClass: "inputFocusGreen",
+    focusPwClass: "inputFocusGreen",
+    showSpinner: false,
+    showMsg: false,
+    msg: ""
   },
   methods: {
-    loginPost() {
-      loginObj.account  = document.getElementById("inputAccount").value;
-      loginObj.password = document.getElementById("inputPassword").value;
-      axios.post(postURL, loginObj)
-        .then(function (response) {
-          console.log(response);
-          if (testMode)
-            tmpObj = JSON.parse(response.data.freeform);
-          else
-            tmpObj = response.data;
-          console.log(tmpObj);
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
+    LoginPost() {
+      app1.showMsg = false;
+      if (document.getElementById("inputAcct").validity.valid
+         && document.getElementById("inputPw").validity.valid)
+      {
+        app1.showSpinner = true;
+        loginObj.account  = document.getElementById("inputAcct").value;
+        loginObj.password = document.getElementById("inputPw").value;
+        axios.post(postURL, loginObj)
+          .then(function (response) {
+            app1.showSpinner = false;
+            console.log(response);
+            if (testMode) tmpObj = JSON.parse(response.data.freeform);
+            else tmpObj = response.data;
+            console.log(tmpObj);
+            if (tmpObj.stats == "success")
+            {
+              app1.msg = "Success!"
+              app1.showMsg = true;
+              setTimeout(loginSuccess, 3000);
+            }
+            else
+            {
+              app1.msg = "Account and password are incorrect!"
+              app1.showMsg = true;
+            }
+          })
+          .catch(function (error) {
+            app1.showSpinner = false;
+            app1.msg = "Network error! Please try again."
+            app1.showMsg = true;
+            console.log(error);
+          })
+      }
+      else
+      {
+        app1.msg = "Input is invalid!"
+        app1.showMsg = true;
+      }
+    },
+    ValidateAcct() {
+      var el = document.getElementById("inputAcct");
+      if (el.validity.valid)
+      {
+        el.parentElement.style.borderColor = "#e6e6e6";
+        this.focusAcctClass = "inputFocusGreen";
+      }
+      else
+      {
+        el.parentElement.style.borderColor = "red";
+        this.focusAcctClass = "inputFocusRed";
+      }
+    },
+    ValidatePw() {
+      var el = document.getElementById("inputPw");
+      if (el.validity.valid)
+      {
+        el.parentElement.style.borderColor = "#e6e6e6";
+        this.focusPwClass = "inputFocusGreen";
+      }
+      else
+      {
+        el.parentElement.style.borderColor = "red";
+        this.focusPwClass = "inputFocusRed";
+      }
     }
   }
 })
@@ -41,4 +97,15 @@ var app1 = new Vue({
 window.onload = function() {
   document.getElementsByTagName("html")[0].style.visibility = "visible";
   this.app1.showContent = true;
+}
+
+
+// Login System
+
+
+function loginSuccess() {
+  localStorage.setItem("who", tmpObj.who);
+  localStorage.setItem("userName", tmpObj.userName);
+  localStorage.setItem("hash", tmpObj.hash);
+  window.location.replace("problem/p000");
 }
--- djangogirls/mysite/templates/login/login.md
+++ djangogirls/mysite/templates/login/login.md
@@ -1,4 +1,16 @@
-
+### 2019/11/25 Validation Message
+	html js add login message and spinner
+### 2019/11/25 Validation and Naming
+	html js
+	revise validate functions and improve readability
+	postURL = ""
+	change path of external js in login.html
+### 2019/11/24 Validation
+	html css js change color to red when value is invalid
+### 2019/11/22 Redirect
+	js store data and redirct after login
+### 2019/11/18 load static
+	html {% load static %}
 ### 2019/11/18 Server-Specific File 
 	html
 ### 2019/11/18/ Post
