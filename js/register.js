df1a191c77becc5d7b89fb2adc9c4a19e783c9f0
BeePunch <kevinappledog@gmail.com>
2019-11-30 11:39:07 +0800

    2019/11/30
    微修改兩頁面樣式

--- js/register.js
+++ js/register.js
@@ -1,3 +1,14 @@
 window.onload = () => {
   new Vue({ el: '#app' })
 }
+
+var submitObj = {
+  "action": "register",
+  "who" : "teacher"||"student",
+  "account" : "account_name",
+  "password" : "123456789",
+  "email" : "123456789@fsdfsdf.com"
+}
+var postURL = "submit/"
+var testMode = false
+var tmpObj = {}
