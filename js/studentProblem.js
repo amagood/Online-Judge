34fc9dc294fe9e36896efd15524fbbfb7846534a
shin-han <hannah880708@gmail.com>
2019-12-11 23:39:06 +0800

    stuPro Update filter.Set up questionLibObj

--- js/studentProblem.js
+++ js/studentProblem.js
@@ -42,8 +42,8 @@ var questionLibObj = {
   "questionNum": "2",//there are twenty question in one page. 一頁幾行
   "questionPage": "1",//1 means select the top 20 question.當前頁數
   "sequence": "id",//排序方式,API少升降序
-  "tag": "loop",
-  "degree": "easy",
+  "tag": "Tag",//filter
+  "degree": "Degree",//filter
   "userName": "amagood",
   "Class": "CSIE110",
   "hash": "A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9"
@@ -56,14 +56,14 @@ window.onload = () => {
     el: "#probapp",
     data() {
       return {
-        Target: "Target",//for dropdown buttom
+        Tag: "Tag",//for dropdown buttom
         Degree: "Degree",//for dropdown buttom
         sortBy: "id",//排序方式
         sortDesc: false,//false:升序
         //perPage: 2,//一頁幾行
         currentPage: 1,//當前頁數
         totalRows:1,//總行數
-        filter: null,//Search
+        filter: null,//text in filter
         fields: [
           {
             key: "id",
@@ -119,6 +119,7 @@ window.onload = () => {
       this.getQuestionData()
     },//created end
     updated(){
+      //---updata questionLibObj---
       //console.log(questionLibObj.questionPage)
       if(this.currentPage!=questionLibObj.questionPage||this.sortBy!=questionLibObj.sequence){
         questionLibObj.questionPage=this.currentPage.toString()//轉字串
@@ -127,6 +128,16 @@ window.onload = () => {
       }
       //console.log(questionLibObj.questionPage)
       //console.log(this.sortBy,this.sortDesc)
+
+      //---clear filter---
+      //console.log(this.filter)
+      if(!this.filter){
+        this.Degree = "Degree"
+        this.Tag = "Tag"
+        questionLibObj.tag=this.Tag
+        questionLibObj.degree=this.Degree
+        console.log(questionLibObj)
+      }
     },
     methods: {
       //mainblock2
@@ -135,7 +146,7 @@ window.onload = () => {
         .post("https://httpbin.org/response-headers?freeform=%7B%20%20%20%22questionLib%22%3A%20%5B%20%20%20%20%20%7B%20%22id%22%3A%20%22a001%22%2C%20%22title%22%3A%20%22title01%22%2C%20%22tag%22%3A%20%22loop%22%2C%20%22degree%22%3A%20%22easy%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%22100%22%2C%20%22inputTime%22%3A%20%2220190101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a002%22%2C%20%22title%22%3A%20%22title01%22%2C%20%22tag%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22hard%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%220%22%2C%20%22inputTime%22%3A%20%2220180101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a003%22%2C%20%22title%22%3A%20%22title01%22%2C%20%22tag%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22easy%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%2250%22%2C%20%22inputTime%22%3A%20%2220170101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a004%22%2C%20%22title%22%3A%20%22title01%22%2C%20%22tag%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22hard%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%2211%22%2C%20%22inputTime%22%3A%20%2220160101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a005%22%2C%20%22title%22%3A%20%22title01%22%2C%20%22tag%22%3A%20%22loop%22%2C%20%22degree%22%3A%20%22mid%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%221%22%2C%20%22inputTime%22%3A%20%2220150101%22%20%7D%20%20%20%5D%2C%20%20%20%22userName%22%20%3A%20%22amagood%22%2C%20%20%20%22Class%22%20%3A%20%22CSIE110%22%2C%20%20%20%22hash%22%20%3A%20%22A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9%22%20%7D%20",questionLibObj)
         .then(response => {
             //console.log(response.data.freeform)
-            tmpobj = JSON.parse(response.data.freeform);
+            tmpobj = JSON.parse(response.data.freeform)
             //console.log(tmpobj.questionLib)
             //console.log(this.items)
             this.items = tmpobj.questionLib
@@ -145,21 +156,25 @@ window.onload = () => {
             //console.log(this.items.length)
           })
           .catch(function (error) {
-            console.log(error);
+            console.log(error)
           });
       },
       problemLink(value){
         return `${value}`
       },
       //mainblock1
-      clickTarget(Target) {
-        this.Target = Target;
-        this.Degree = "Degree";
+      clickTag(Tag) {
+        this.Tag = Tag
+        this.Degree = "Degree"
+        questionLibObj.tag=this.Tag
+        console.log(questionLibObj)
       },
       clickDegree(Degree) {
-        this.Degree = Degree;
-        this.Target = "Target";
+        this.Degree = Degree
+        this.Tag = "Tag"
+        questionLibObj.degree=this.Degree
       },
+      //mainblock3
       onFiltered(filteredItems) {
         // Trigger pagination to update the number of buttons/pages due to filtering
         this.totalRows = filteredItems.length
