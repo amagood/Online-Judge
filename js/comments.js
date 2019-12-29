d6139b47eda9970d2307ef5ad1b716ff6381c64a
charlieyang <402057@gmail.com>
2019-12-30 02:20:48 +0800

    comments,rank移除註冊和登入按鈕, 修正rank,index路徑連結

--- js/comments.js
+++ js/comments.js
@@ -44,14 +44,10 @@ var navapp = new Vue({
 		userid : "",
 		name : "",
 		isShow : false, //題目庫顯示
-		regIsShow : false, //註冊頁顯示
-		logIsShow : true, //登入頁顯示
 		whichShow : "",
 	},
 	created() {
 		this.chooseProblems()
-    this.canRegister()
-    this.canLogin()
 	},
 	methods: {
 		chooseProblems(){
@@ -65,20 +61,6 @@ var navapp = new Vue({
 			else if(self.userid === "student"){
 				self.isShow = true
 				self.whichShow = "student"
-			}
-		},
-		canRegister(){
-			let self = this
-			self.userid = localStorage.getItem("who")
-			if(self.userid === "admin"){
-				self.regIsShow = true
-			}
-    },
-    canLogin(){
-			let self = this
-			self.userid = localStorage.getItem("userName")
-			if(self.userid){
-				self.logIsShow = false
 			}
 		},
 		clearStorage(){
--- js/ranking.js
+++ js/ranking.js
@@ -77,14 +77,10 @@ var navapp = new Vue({
 		userid : "",
 		name : "",
 		isShow : false, //題目庫顯示
-		regIsShow : false, //註冊頁顯示
-		logIsShow : true, //登入頁顯示
 		whichShow : "",
 	},
 	created() {
 		this.chooseProblems()
-    this.canRegister()
-    this.canLogin()
 	},
 	methods: {
 		chooseProblems(){
@@ -98,20 +94,6 @@ var navapp = new Vue({
 			else if(self.userid === "student"){
 				self.isShow = true
 				self.whichShow = "student"
-			}
-		},
-		canRegister(){
-			let self = this
-			self.userid = localStorage.getItem("who")
-			if(self.userid === "admin"){
-				self.regIsShow = true
-			}
-    },
-    canLogin(){
-			let self = this
-			self.userid = localStorage.getItem("userName")
-			if(self.userid){
-				self.logIsShow = false
 			}
 		},
 		clearStorage(){
