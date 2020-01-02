a66fab2a71344b6932cf0b1b714a71b46efb22fc
shin-han <hannah880708@gmail.com>
2020-01-02 19:53:10 +0800

    stuPro add classList

--- js/studentProblem.js
+++ js/studentProblem.js
@@ -46,6 +46,16 @@ var tmpobj = {}
 var postURL = ""
 //test postURL="https://httpbin.org/response-headers?freeform=%7B%20%20%20%22questionLib%22%3A%20%5B%20%20%20%20%20%7B%20%22id%22%3A%20%22a001%22%2C%20%22title%22%3A%20%22title01%22%2C%20%22target%22%3A%20%22loop%22%2C%20%22degree%22%3A%20%22easy%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%22100%22%2C%20%22inputTime%22%3A%20%2220190101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a002%22%2C%20%22title%22%3A%20%22title02%22%2C%20%22target%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22hard%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%220%22%2C%20%22inputTime%22%3A%20%2220180101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a003%22%2C%20%22title%22%3A%20%22title03%22%2C%20%22target%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22easy%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%2250%22%2C%20%22inputTime%22%3A%20%2220170101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a004%22%2C%20%22title%22%3A%20%22title04%22%2C%20%22target%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22hard%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%2211%22%2C%20%22inputTime%22%3A%20%2220160101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a005%22%2C%20%22title%22%3A%20%22title05%22%2C%20%22target%22%3A%20%22loop%22%2C%20%22degree%22%3A%20%22mid%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%221%22%2C%20%22inputTime%22%3A%20%2220150101%22%20%7D%20%20%20%5D%2C%20%20%20%22userName%22%20%3A%20%22amagood%22%2C%20%20%20%22Class%22%20%3A%20%22CSIE110%22%2C%20%20%20%22hash%22%20%3A%20%22A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9%22%20%7D"
 
+//使用者所有班級
+var classList={
+  "action" : "getClassList",
+  "userName" : "amagood",
+  "hash" : "A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9"
+}
+var classPostURL=""
+//test var classPostURL="https://httpbin.org/response-headers?freeform=%20%20%7B%20%20%20%20%20%22classList%22%3A%5B%5D%2C%20%20%20%20%20%22userName%22%20%3A%20%22amagood%22%2C%20%20%20%20%20%22hash%22%20%3A%20%22A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9%22%20%20%20%7D"
+var classTmpobj={}
+
 /*problem link
 Vue.component("prob-link", {
   delimiters: ['${', '}'],
@@ -57,8 +67,10 @@ var probapp = new Vue({
   delimiters: ['${', '}'],
   el: "#probapp",
   data: {
+    classList:[],//存class name list
+    classButtonText: "Class",
     noGetData: true,//loading
-    noFilterData: false,//判斷filter
+    filterData: false,//判斷filter
     Tag: "tag",//for dropdown buttom
     Degree: "degree",//for dropdown buttom
     filter: null,//text in filter
@@ -119,6 +131,7 @@ var probapp = new Vue({
   },//computed end
   created() {
     this.getQuestionData()
+    this.getClassList()
   },//created end
   updated() {
     //---updata questionLibObj for changePage---
@@ -139,7 +152,7 @@ var probapp = new Vue({
     }
 
     //---clear filter---
-    if (!this.filter && this.noFilterData) {
+    if (!this.filter && this.filterData) {
       this.noGetData = true
       this.Degree = "degree"
       this.Tag = "tag"
@@ -148,7 +161,7 @@ var probapp = new Vue({
       //console.log(questionLibObj)
       this.getQuestionData()
       console.log("clear,get all data")
-      this.noFilterData = false
+      this.filterData = false
     }
   },//updated end
   methods: {
@@ -199,7 +212,7 @@ var probapp = new Vue({
       questionLibObj.degree = "degree"
       if (Tag != "tag") {
         this.noGetData = true
-        this.noFilterData = true
+        this.filterData = true
         //console.log(questionLibObj)
         this.getQuestionData()
         console.log("tag sort get data")
@@ -212,7 +225,7 @@ var probapp = new Vue({
       questionLibObj.target = "tag"
       if (Degree != "degree") {
         this.noGetData = true
-        this.noFilterData = true
+        this.filterData = true
         //console.log(questionLibObj)
         this.getQuestionData()
         console.log("degree sort get data")
@@ -223,12 +236,50 @@ var probapp = new Vue({
       // Trigger pagination to update the number of buttons/pages due to filtering
       this.totalRows = filteredItems.length
       this.currentPage = 1
+    },
+    getClassList(){
+      axios
+        .post(classPostURL, classList)
+        .then(function (response) {
+          //console.log(response.data)
+          //console.log(JSON.parse(response.data.freeform))//test
+          classTmpobj =response.data
+          probapp.classList=classTmpobj.classList
+          console.log(probapp.classList)
+          console.log("get class list from classPostURL")
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    },
+    clickClass(chooseClass){
+      console.log(chooseClass)//傳入班級
+      if (chooseClass != "All Problem") {
+        probapp.classButtonText = chooseClass
+        questionLibObj.Class= chooseClass
+        probapp.noGetData = true
+        //postURL="https://httpbin.org/response-headers?freeform=%7B%20%20%20%20%20%22questionLib%22%3A%20%5B%20%20%20%20%20%20%20%7B%20%22id%22%3A%20%22a001%22%2C%20%22title%22%3A%20%22title01%22%2C%20%22target%22%3A%20%22loop%22%2C%20%22degree%22%3A%20%22easy%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%22100%22%2C%20%22inputTime%22%3A%20%2220190101%22%20%7D%2C%20%20%20%20%20%20%20%7B%20%22id%22%3A%20%22a003%22%2C%20%22title%22%3A%20%22title03%22%2C%20%22target%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22easy%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%2250%22%2C%20%22inputTime%22%3A%20%2220170101%22%20%7D%20%20%20%20%20%5D%2C%20%20%20%20%20%22userName%22%20%3A%20%22amagood%22%2C%20%20%20%20%20%22Class%22%20%3A%20%22CSIE110%22%2C%20%20%20%20%20%22hash%22%20%3A%20%22A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9%22%20%20%20%7D"
+        //test postURL (class problem)
+        //console.log(questionLibObj)
+        probapp.getQuestionData()
+        console.log("class get class data")
+      }
+      else{
+        probapp.noGetData = true
+        probapp.classButtonText = "Class"
+        questionLibObj.Class= chooseClass
+        //postURL="https://httpbin.org/response-headers?freeform=%7B%20%20%20%22questionLib%22%3A%20%5B%20%20%20%20%20%7B%20%22id%22%3A%20%22a001%22%2C%20%22title%22%3A%20%22title01%22%2C%20%22target%22%3A%20%22loop%22%2C%20%22degree%22%3A%20%22easy%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%22100%22%2C%20%22inputTime%22%3A%20%2220190101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a002%22%2C%20%22title%22%3A%20%22title02%22%2C%20%22target%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22hard%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%220%22%2C%20%22inputTime%22%3A%20%2220180101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a003%22%2C%20%22title%22%3A%20%22title03%22%2C%20%22target%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22easy%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%2250%22%2C%20%22inputTime%22%3A%20%2220170101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a004%22%2C%20%22title%22%3A%20%22title04%22%2C%20%22target%22%3A%20%22array%22%2C%20%22degree%22%3A%20%22hard%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%2211%22%2C%20%22inputTime%22%3A%20%2220160101%22%20%7D%2C%20%20%20%20%20%7B%20%22id%22%3A%20%22a005%22%2C%20%22title%22%3A%20%22title05%22%2C%20%22target%22%3A%20%22loop%22%2C%20%22degree%22%3A%20%22mid%22%2C%20%22percentagePassing%22%3A%20%2250%22%2C%20%22respondent%22%3A%20%221%22%2C%20%22inputTime%22%3A%20%2220150101%22%20%7D%20%20%20%5D%2C%20%20%20%22userName%22%20%3A%20%22amagood%22%2C%20%20%20%22Class%22%20%3A%20%22CSIE110%22%2C%20%20%20%22hash%22%20%3A%20%22A7FCFC6B5269BDCCE571798D618EA219A68B96CB87A0E21080C2E758D23E4CE9%22%20%7D"
+        //test postURL (all problem)
+        //console.log(questionLibObj)
+        probapp.getQuestionData()
+        console.log("clear class,get all data")
+      }
     }
   },//methods end
 })
 
 
 window.onload = () => {
-  document.getElementById("problemLib").className = "problemLib w3-animate-opacity";
+  document.getElementById("probapp").className = "problemLib w3-animate-opacity";
   document.getElementsByTagName("html")[0].style.visibility = "visible";
 }
