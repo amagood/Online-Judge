2d51506479d9166f941235409ef3e4445b23e5f0
Stevenxkh <xkhsky@gmail.com>
2020-01-07 02:41:03 +0800

    install files for final version

--- OJ_test/Rank/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RankConfig(AppConfig):
-    name = 'Rank'
--- OJ_test/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/steven/Downloads/軟工/Online-Judge-back-end-Stevenxkh/OJ_test/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
